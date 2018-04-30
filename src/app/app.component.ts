2c06ef572b89c43fd21801910547cf3696b55567
StiglitzD <stigltzdev@protonmail.com>
2018-04-30 18:49:20 +0200

    Validé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'Posts';
+    posts= [ 
+        {
+            title: 'Mon premier post',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  
+            loveIts: 2,
+            created_at: new Date()
+        },
+        {   
+            title: 'Mon deuxième post',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  
+            loveIts: -1,
+            created_at:new Date()
+        },
+        {
+            title: 'Encore un post',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  
+            loveIts: 0,
+            created_at:new Date()
+        }
+    ]
 }
---
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list-item-component',
+    templateUrl: './post-list-item-component.component.html',
+    styleUrls: ['./post-list-item-component.component.scss']
+})
+export class PostListItemComponentComponent implements OnInit {
+    @Input() Title;
+    @Input() Content:string;
+    @Input() Created_at: Date;
+    @Input() Likes: var;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+        
+    onAime(){
+        this.Likes=this.Likes +1
+    }
+    onAimePas(){
+        this.Likes=this.Likes -1
+    }
+
+}
